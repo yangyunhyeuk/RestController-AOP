44d78c3e6b3473c2a1c2cef7d2e6f393579a5b6e
azz4622@naver.com <azz4622@naver.com>
2022-01-20 16:41:09 +0900

    Valid_AOP_Log

--- src/main/java/com/yang/test/domain_model/UpdateReqDto.java
+++ src/main/java/com/yang/test/domain_model/UpdateReqDto.java
@@ -2,8 +2,11 @@ package com.yang.test.domain_model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UpdateReqDto {
+    @NotBlank(message = "password가 입력되지 않았습니다")
     private String password;
     private String phone;
 }
